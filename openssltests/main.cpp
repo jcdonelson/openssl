2aa2abd5af27330350fccfdb46ad10cf49e30ff9
JimD <git@jimdonelson.com>
2014-06-01 22:31:53 -0400

    Initial Commit

---
+++ openssltests/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  openssltests
+//
+//  Created by james donelson on 6/1/14.
+//  Copyright (c) 2014 james donelson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
